5ea00b60332f4106f47ffa8ee61854041b7a5174
vinycius monteiro <vmonteiro@proadv.adv.br>
2020-07-24 19:01:57 -0300

    Adicionado o componente do carrinho ao modulo Main.

--- src/app/main/main.module.ts
+++ src/app/main/main.module.ts
@@ -3,11 +3,12 @@ import { MainRoutingModule } from './main-routing.module';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MainComponent } from './main.component';
+import { CarrinhoComponent } from './carrinho/carrinho.component';
 
 
 
 @NgModule({
-  declarations: [MainComponent],
+  declarations: [MainComponent, CarrinhoComponent],
   imports: [
     CommonModule,
     MainRoutingModule
