bd42328113e89d7e7abd8cb9cf71deb23ce15746
vinycius monteiro <vinyciussilvamonteiro@gmail.com>
2020-07-26 18:31:24 -0300

    Commit

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ImagemLoaderDirective } from './directives/imagem-loader.directive';
+import { ModalModule } from 'ngx-bootstrap';
 
 
 
 @NgModule({
   declarations: [ImagemLoaderDirective],
   imports: [
-    CommonModule
+    CommonModule,
+    ModalModule.forRoot()
   ],
   exports: [ImagemLoaderDirective]
 })
